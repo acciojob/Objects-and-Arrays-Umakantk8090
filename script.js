b785ac156b8f0b477b5b36d36d981a515a6d3fdb
Umakantk8090 <117897072+Umakantk8090@users.noreply.github.com>
2023-10-12 11:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-let team = players;
-let team1 = [...players];
-let cap1=person;
-let cap1 = [...person];
+let team = players
+let team1 = players.slice();
+
+let cap1 = person.slice();
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
